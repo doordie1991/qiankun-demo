a7785a9ee5b6c1514172260e03d4560e6149726e
‘doordie1991’ <doordie1991@163.com>
2022-03-10 01:13:04 +0800

    测试2

--- qk-main/src/api/service/auth.js
+++ qk-main/src/api/service/auth.js
@@ -4,7 +4,7 @@ export default (name) => {
   const login = (params) => {
     return $http.post("/login", params)
   }
-  //获取用户信息
+  //获取用户信息 
   const getInfo = () => {
     return $http.get("/getInfo")
   }
